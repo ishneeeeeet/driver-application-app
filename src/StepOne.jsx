ef4572f3774c9573ef20c56442cdb53cde8740a4
Ishneet Singh <ishneet100@gmail.com>
2023-08-04 16:17:26 -0600

    modified:   src/StepOne.jsx

--- src/StepOne.jsx
+++ src/StepOne.jsx
@@ -38,6 +38,7 @@ const StepOne = ({ onNext }) => {
     questionFour: "",
     questionFive: "",
   });
+ 
 
   const [errors, setErrors] = useState({});
 
@@ -148,13 +149,14 @@ const StepOne = ({ onNext }) => {
                 id="email"
                 name="email"
                 type="email"
+                onChange={handleChange}
                 autoComplete="email"
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </Form.Control>
           </Form.Field>
 
-          {/* Date of Birth Field */}
+          
           <div className="sm:col-span-3">
             <label
               htmlFor="date-of-birth"
