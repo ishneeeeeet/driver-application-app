f8d6c7ad431e9f57097dc66297948aa87121eb21
Ishneet Singh <ishneet100@gmail.com>
2023-08-05 21:32:11 -0600

    modified:   src/StepFour.jsx

--- src/StepFour.jsx
+++ src/StepFour.jsx
@@ -35,7 +35,7 @@ const StepFour = () => {
         type="submit"
         className="box-border w-full text-violet11 shadow-blackA7 hover:bg-mauve3 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-white px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none mt-[10px]"
       >
-        Next
+        Submit
       </button>
     </form>
   );
