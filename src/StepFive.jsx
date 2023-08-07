8e2e67b817a0ff81f5889c270bee1b8baf5d580b
Ishneet Singh <ishneet100@gmail.com>
2023-08-06 20:34:53 -0600

    modified:   package-lock.json 	modified:   package.json 	modified:   src/StepFive.jsx 	modified:   src/index.js

--- src/StepFive.jsx
+++ src/StepFive.jsx
@@ -36,6 +36,7 @@ const StepFive = () => {
       >
         Submit
       </button>
+      {console.log(form)}
     </form>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { FormDataContext } from "./context";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const rootElement = document.getElementById("root");
 
