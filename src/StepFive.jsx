f107877d73cc32b1633f001c5eef7b83ecb7e7ea
Ishneet Singh <ishneet100@gmail.com>
2023-08-07 13:23:39 -0600

    modified:   src/StepFive.jsx

--- src/StepFive.jsx
+++ src/StepFive.jsx
@@ -1289,23 +1289,7 @@ const StepFive = () => {
           </div>
         </div>
 
-        <div className="sm:col-span-3">
-          <label
-            htmlFor="last-name"
-            className="block text-sm font-medium leading-6 text-gray-900"
-          >
-            Last name
-          </label>
-          <div className="mt-2">
-            <input
-              type="text"
-              name="lastName"
-              id="last-name"
-              autoComplete="family-name"
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
+        
       </div>
                        
       <button
